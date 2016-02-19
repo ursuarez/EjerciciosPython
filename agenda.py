edfe93571ad6fe7f217aee8a90fcf1a74ff22686
ursuarez <ur.suarez@gmail.com>
2016-02-18 21:44:37 -0600

    Agenda aplicando PEP8

--- agenda.py
+++ agenda.py
@@ -7,38 +7,38 @@ numero_contactos = input("\n\n\n\nCuantos contactos desea agregar?: ")
 nombre_archivo = raw_input("\nQue nombre desea darle a su archivo?: ")
 
 nombre = nombre_archivo + ".txt"
-archivo = open(nombre,"w")
-archivo.write ('\t' + nombre_archivo.upper())
+archivo = open(nombre, "w")
+archivo.write('\t' + nombre_archivo.upper())
 
-sus = "-" *35
+sus = "-" * 35
 
-for contador in range (numero_contactos):
-	num_contador = contador + 1
+for contador in range(numero_contactos):
+    num_contador = contador + 1
 	print "\n", sus, "\n"
-	archivo.write ('\n\n' + sus + '\n\n')
+	archivo.write('\n\n' + sus + '\n\n')
 
-	print "Contacto #%d : \n" %num_contador
-	archivo.write ('Contacto # ' + str(num_contador) + '\n')
-	
+	print "Contacto #%d : \n" % num_contador
+	archivo.write('Contacto # ' + str(num_contador) + '\n')
+
 	nombre = raw_input("\nNombre: ")
-	archivo.write ('\nNombre: ' + nombre)
+	archivo.write('\nNombre: ' + nombre)
 
 	apellidop = raw_input("\nApellido Paterno: ")
-	archivo.write ('\nApellido Paterno: ' + apellidop)
+	archivo.write('\nApellido Paterno: ' + apellidop)
 
 	apellidom = raw_input("\nApellido Materno: ")
-	archivo.write ('\nApellido Materno: ' + apellidom)
+	archivo.write('\nApellido Materno: ' + apellidom)
 
 	telefono = raw_input("\nTel: ")
-	archivo.write ('\nTel: ' + telefono)
+	archivo.write('\nTel: ' + telefono)
 
 	Email = raw_input("\nEmail: ")
-	archivo.write ('\nEmail: ' + Email)
+	archivo.write('\nEmail: ' + Email)
 
 
 print "\n\nGuardando archivo "
-for cont in range (3):
+for cont in range(3):
 	print ". "
-	sleep(0.5)
-
+    sleep(0.5)
+    
 archivo.close()
