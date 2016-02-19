26d44cc5b81f032537a0c1ea27bfdb9cb4245403
ursuarez <ur.suarez@gmail.com>
2016-02-18 20:59:51 -0600

    Agenda

---
+++ agenda.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*-coding:utf-8-*-
+
+
+numero_contactos = input("\n\n\n\nCuantos contactos desea agregar?: ")
+nombre_archivo = raw_input("\nQue nombre desea darle a su archivo?: ")
+
+nombre = nombre_archivo + ".txt"
+archivo = open(nombre,"w")
+# archivo.write ("Hola mundo")
+# archivo.close()
+
+sus = "-" *35
+
+for contador in range (numero_contactos):
+	num_contador = contador + 1
+	print "\n", sus, "\n"
+	archivo.write ('\n' + sus + '\n')
+
+	print "Contacto #%d : \n" %num_contador
+	archivo.write ('Contacto # ' + str(num_contador) + '\n')
+	#raw_input
+
+archivo.close()
+
+
