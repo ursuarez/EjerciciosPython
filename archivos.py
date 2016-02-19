01d2077643d1d305db90983b961fd06287d7ec47
ursuarez <ur.suarez@gmail.com>
2016-02-18 20:36:50 -0600

    Manejo de archivos

--- archivos.py
+++ archivos.py
@@ -1,8 +1,9 @@
 nombre_archivo = raw_input("\n\nQue nombre deseas darle a tu archivo?: ")
 
 nombre = nombre_archivo + ".txt"
-archivo = open(nombre,"wb")
-archivo.write ("Hola mundo")
+archivo = open(nombre,"w")
+archivo.write('Hola mundo\n')
+archivo.write('El nombre del archivo es: ' + nombre) 
 archivo.close()
 
 archivo = open(nombre,"r+")
