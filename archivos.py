b0fa2719c14d8744fd65595a5b0747d96d453c55
ursuarez <ur.suarez@gmail.com>
2016-02-18 20:22:21 -0600

    Archivos.py

--- archivos.py
+++ archivos.py
@@ -1,8 +1,11 @@
-archivo = open("foo.txt","wb")
+nombre_archivo = raw_input("\n\nQue nombre deseas darle a tu archivo?: ")
+
+nombre = nombre_archivo + ".txt"
+archivo = open(nombre,"wb")
 archivo.write ("Hola mundo")
 archivo.close()
 
-archivo = open("foo.txt","r+")
+archivo = open(nombre,"r+")
 hola = archivo.read()
 print hola
 archivo.close()
